422b8bf12359862c7b02174792aaf359bb31c040
liyuqi <am997vivian@gmail.com>
2014-02-25 18:47:20 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import json
+import urllib2
+import urllib
+import traceback
+import threading
+from lxml import etree
+
+fan_url = 'https://www.facebook.com/PanSci'
+test_url = 'https://www.facebook.com/PanSci/posts/10202281683458386'
+fb_url = 'https://www.facebook.com'
+Pan_url = ''
+RESULT = {}
+NOT_FOUND = []
+
+def get_like_id():
+	ret = []
+	f = urllib2.urlopen(test_url)
+	parser = etree.HTMLParser()
+	tree = etree.parse(f, parser)
+	cells = tree.xpath('//a[@class="_5rwn"]')
+    
+	for line in cells:
+		print("1hello")
+		Pan_url = line.xpath('@href').text
+		print("2hello")
+		print(Pan_url) #http://pansci.tw/archives/56822
+	#return Pan_url
+    
+while 1:
+    get_like_id()
